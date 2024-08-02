4ce4374d6d5ecbe23dd29e8319b918a6fbf6c70f
Franco Bonafina <franbonafina1@gmail.com>
2024-08-02 13:21:04 -0300

    Create SumWallet.java

---
+++ java-project/src/main/java/com/yourself/SumWallet.java
@@ -0,0 +1,15 @@
+// { autofold
+package com.yourself;
+
+import java.util.Arrays;
+
+public class SumWallet {
+// }
+
+public static int sum(int A, int B) {
+	return A + ?; // Fix HERE  
+}
+
+//{ autofold
+}
+//}

87eea0e6eee30a95b08f164acf8aa9b27907e5b3
Franco Bonafina <franbonafina1@gmail.com>
2024-08-02 13:13:16 -0300

    Create Conditional.java

---
+++ java-project/src/main/java/com/yourself/Conditional.java
@@ -0,0 +1,22 @@
+// { autofold
+package com.yourself;
+
+import java.util.Arrays;
+
+public class Conditional {
+// }
+
+public static int conditional(int... galaxies) {
+	int conditional = ?; // COMPLETE HERE
+
+  if(conditional) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+//{ autofold
+}
+//}

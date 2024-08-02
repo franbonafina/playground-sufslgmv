479d012b6a5db3f3ab67d9b822c852e10d93b884
Franco Bonafina <franbonafina1@gmail.com>
2024-08-02 13:37:19 -0300

    Update Conditional.java

--- java-project/src/main/java/com/yourself/Conditional.java
+++ java-project/src/main/java/com/yourself/Conditional.java
@@ -7,14 +7,14 @@ public class Conditional {
 // }
 
 public static int conditional(int... galaxies) {
-	int conditional = ?; // COMPLETE HERE
-
-  if(conditional) {
-    return true;
-  }
-  else {
-    return false;
-  }
+	String conditional = ""; // COMPLETE HERE
+	
+	if(conditional=="TRUE") {
+    		return true;
+  	}
+  	else {
+    		return false;
+ 	}
 }
 
 //{ autofold
